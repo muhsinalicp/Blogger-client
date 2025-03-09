bd2870c7027c33485bad4b1a5112e8ec0d077b6f
muhsinalicp <122996567+muhsinalicp@users.noreply.github.com>
2025-03-09 16:51:31 +0530

    fixed credentials bug

--- src/queryOptions/signIn.js
+++ src/queryOptions/signIn.js
@@ -5,13 +5,7 @@ const REACT_APP_BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 export async function signInUser(userData) {
     try {
-        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`,{
-            withCredentials: true,
-            // headers: {
-            //     'Access-Control-Allow-Origin': '*', 
-            //     'Content-Type': 'application/json'
-            // }
-        }, userData);
+        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`,{withCredentials: true}, userData);
         return data;
     } catch (error) {
         throw error.response?.data || { message: "Something went wrong" };
