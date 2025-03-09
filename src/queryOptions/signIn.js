8098915c413c92c8b1d46828ddfaa53e057a0817
muhsinalicp <122996567+muhsinalicp@users.noreply.github.com>
2025-03-09 16:35:41 +0530

    added with credentials

--- src/queryOptions/signIn.js
+++ src/queryOptions/signIn.js
@@ -5,7 +5,13 @@ const REACT_APP_BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 export async function signInUser(userData) {
     try {
-        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`, userData);
+        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`,{
+            withCredentials: true,
+            // headers: {
+            //     'Access-Control-Allow-Origin': '*', 
+            //     'Content-Type': 'application/json'
+            // }
+        }, userData);
         return data;
     } catch (error) {
         throw error.response?.data || { message: "Something went wrong" };
