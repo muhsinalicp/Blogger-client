92b03b94bb05f2882759267915811fbee82dd57d
muhsinalicp <122996567+muhsinalicp@users.noreply.github.com>
2025-03-10 21:46:56 +0530

    fixing input errors

--- src/queryOptions/signIn.js
+++ src/queryOptions/signIn.js
@@ -5,7 +5,7 @@ const REACT_APP_BASE_URL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 export async function signInUser(userData) {
     try {
-        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`,{withCredentials: true}, userData);
+        const { data } = await axios.post(`${REACT_APP_BASE_URL}/auth/login`, userData , {withCredentials: true});
         return data;
     } catch (error) {
         throw error.response?.data || { message: "Something went wrong" };
